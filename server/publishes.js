8d62d3f3c2163aaa6280974c99ab946c75a74407
davis <dbh937@gmail.com>
2015-03-20 23:14:51 -0400

    removed autopublish and added publish behavior for assignments.

---
+++ server/publishes.js
@@ -0,0 +1,21 @@
+/**
+ * Created by davis on 3/20/15.
+ */
+
+Meteor.publish('assignments', function() {
+  if (Roles.userIsInRole(this.userId, 'admin')) // if they're an admin, let 'em see everything.
+    return Assignments.find({});
+  else { // only show students assignments they've been assigned.
+    var c = Courses.find({students: {username: Meteor.users.findOne(this.userId).username}}).fetch();
+    var a = [];
+    for (var i=0;i < c.length;i++) // get *all* of the assignments that are relevant in one object
+      a.concat(c[i]);
+    return Assignments.find({_id: {$in: a}}); // return all of those objects.
+  }
+});
+
+Meteor.publish('courses', function() {
+  // only give access to course list if they're an admin.
+  if (Roles.userIsInRole(this.userId, 'admin'))
+    return Courses.find({});
+});
