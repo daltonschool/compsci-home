8680d218a3c216edffcb2b0ad325169cba1477ca
davis <dbh937@gmail.com>
2015-03-20 23:26:20 -0400

    students now only see assignments that are assigned to them.

--- server/publishes.js
+++ server/publishes.js
@@ -6,11 +6,14 @@ Meteor.publish('assignments', function() {
   if (Roles.userIsInRole(this.userId, 'admin')) // if they're an admin, let 'em see everything.
     return Assignments.find({});
   else { // only show students assignments they've been assigned.
-    var c = Courses.find({students: {username: Meteor.users.findOne(this.userId).username}}).fetch();
-    var a = [];
-    for (var i=0;i < c.length;i++) // get *all* of the assignments that are relevant in one object
-      a.concat(c[i]);
-    return Assignments.find({_id: {$in: a}}); // return all of those objects.
+    if (Meteor.users.findOne(this.userId)) {
+      var c = Courses.find({students: {username: Meteor.users.findOne(this.userId).username}}).fetch();
+      var a = [];
+      for (var i=0;i < c.length;i++) { // get *all* of the assignments that are relevant in one object
+        a = a.concat(c[i].assignments);
+      }
+      return Assignments.find({_id: {$in: a}}); // return all of those objects.
+    }
   }
 });
 
