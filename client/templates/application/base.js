271170516a6122580bf6132f76e621a1a5b8abcc
davis <dbh937@gmail.com>
2015-03-22 10:30:03 -0400

    implemented dalton login

--- client/templates/application/base.js
+++ client/templates/application/base.js
@@ -1,6 +1,9 @@
 /**
  * Created by davis on 3/17/15.
  */
+
+Session.setDefault('warnings', '');
+
 Accounts.ui.config({
   passwordSignupFields: "USERNAME_ONLY"
 });
@@ -10,5 +13,26 @@ Handlebars.registerHelper("title", function(title) {
     document.title = title + ' | dCS';
   } else {
     document.title = "dCS";
+  }
+});
+
+Template.login.helpers({
+  warnings: function() {
+    return Session.get('warnings');
+  }
+});
+Template.login.events({
+  "submit .loginForm": function(e) { // custom login form
+    e.preventDefault();
+    var username = e.target.username.value;
+    var password = e.target.password.value;
+    Meteor.loginWithDalton(username, password, function(error) {
+      if (error) {
+        Session.set("warnings", "Invalid Username or Password.");
+      } else {
+        Session.set("warnings", "");
+      }
+    });
+    return false;
   }
 });
