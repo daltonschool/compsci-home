26e80d9e79f10594ed89cec6ca35d74cfd5099fb
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2015-03-30 12:49:05 -0400

    removed granting admin roles in startup.js (moved to accounts-dalton submodule)

--- server/startup.js
+++ server/startup.js
@@ -3,15 +3,6 @@
  */
 
 Meteor.startup(function() {
-  var u = Meteor.users.findOne({username: "c17dh"});
-  if (u) {
-    Roles.addUsersToRoles(u._id, 'admin');
-  }
-  var c = Meteor.users.findOne({username: "cforster"});
-  if (c) {
-    Roles.addUsersToRoles(c._id, 'admin');
-  }
-
   UploadServer.init({
     tmpDir: process.env.PWD + '/.uploads/tmp',
     uploadDir: process.env.PWD + '/.uploads',
